d7fef7284276795b1660717e29049934295e0104
MicahBear <micah.b.elm@gmail.com>
2022-07-29 12:29:41 -0700

    set up route files and route folder

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,27 @@ const mongoose = require('mongoose')
 
 // route for root.
 app.get('/', (request, response) => {
-    response.json({ message: "testing for task" })
+    response.status(200).json({ message: "Testing for task" })
+})
+
+// route for adding task
+app.post('/addTask', (request, response) => {
+    response.status(200).json({ message: "Testing add task route" })
+})
+
+// route for editing task.
+app.put('/edit:id', (request, response) => {
+    response.status(200).json({ message: "Testing edit of task route" })
+})
+
+//route for completing task
+app.put('/markComplete', (request, response) => {
+    response.status(200).json({ message: "Testing complete task route" })
+})
+
+// route for deleting task
+app.delete('/deleteTask', (request, response) => {
+    response.status(200).json({ message: "Testing complete task route" })
 })
 
 app.listen(port, () => console.log(` Server is started on PORT ${port}`))
